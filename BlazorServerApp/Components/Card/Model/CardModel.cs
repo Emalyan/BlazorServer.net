52d514d283557becfbce65f8e74f1ab476f678d8
Emelyanenko <i.emelyanenko@itransition.corp>
2020-03-26 17:28:34 +0300

    Commit

---
+++ BlazorServerApp/Components/Card/Model/CardModel.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.WebEncoders.Testing;
+
+namespace BlazorServerApp.Components.Card.Model
+{
+    public class CardModel
+    {    
+        public CardModel(int order)
+        {
+            var random = new Random();
+            Id = random.Next().ToString();
+            Order = order;
+        }
+        public string Id { get; }
+        public string Value { get; set; } 
+        
+        public int Order { get; set; }
+
+        public void SetValue(ChangeEventArgs value)
+        {            
+            Value = value.Value.ToString();
+        }
+
+        public void RemoveCard(List<CardModel> cards)
+        {
+            var card = cards.FirstOrDefault(x => x.Id == Id);
+            cards.Remove(card);
+        }
+
+        public void DragEnd(DragEventArgs args)
+        {
+            var test = args;
+        }
+    }
+}
---
+++ BlazorServerApp/Components/Table/Row/Models/RowModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using BlazorServerApp.Components.Card.Model;
+
+namespace BlazorServerApp.Components.Table.Row.Models
+{
+    public class RowModel
+    {
+        public RowModel()
+        {
+            var random = new Random();
+            Id = random.Next().ToString();
+            Cards = new List<CardModel>();
+        }
+        public string Id { get; set; }
+        public List<CardModel> Cards { get; set; }
+    }
+}
